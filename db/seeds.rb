4e4bc1bbcabb477f5a7d39509ffb1f6364bb1ee4
gagan <meesala.gagan@gmail.com>
2016-02-28 07:47:12 +0530

    Minor refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,13 @@
 require 'csv'
 
+puts "Started creating campaigns"
 # Creating Array as Campaign Names
 campaign_array = ["ssss_uk_01B", "Emmerdale", "ssss_uk_02A", "ssss_uk_02B"]
 campaign_array.each do |campaign|
   # Creating campaign object
   Campaign.create(name: campaign)
 end
+puts "Finished Campaigns.........!"
 
 # Reading data from file
 file_data = File.read('/home/gagan/Desktop/votes.txt')
@@ -15,6 +17,7 @@ file_data = File.read('/home/gagan/Desktop/votes.txt')
 # http://stackoverflow.com/questions/8710444/is-there-a-way-in-ruby-1-9-to-remove-invalid-byte-sequences-from-strings
 csv_file = CSV.new(file_data.chars.select(&:valid_encoding?).join, {:col_sep => " "})
 
+puts "Started creating votes associated with campaigns"
 # Converting CSV to Array and creating voting objects
 csv_file.to_a.map do |data|
   key = campaign_array.index(data[2].split(':')[1])
@@ -23,3 +26,4 @@ csv_file.to_a.map do |data|
     Vote.create(validity: data[3].split(':')[1], choice: data[4].split(':')[1], campaign_id: key + 1)
   end
 end
+puts "Finished Votes..........!"
